279eb336c1494a84c42a0717b5c80cdf08954786
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-04 05:18:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Write a program to perform the following operations on a given String 
+# a) Convert all small letters in a string into capital letters
+# b) find the number of occurences of a given substring 
+i=input()
+print(i.upper())
+
+v=input()
+print(i.count(v))
